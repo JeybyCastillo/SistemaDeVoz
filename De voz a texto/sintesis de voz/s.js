4dc29ce819d33ae569b212dfebe8d3120a0f077e
JB <71406452+JeybyCastillo@users.noreply.github.com>
2023-12-12 10:01:31 -0600

    Delete De voz a texto directory

--- De voz a texto/sintesis de voz/s.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('voz').addEventListener("click",()=>{
-    pronunciar(document.getElementById("texto").value);
-});
-
-function pronunciar(texto){
-    speechSynthesis.speak(new SpeechSynthesisUtterance(texto));
-}
